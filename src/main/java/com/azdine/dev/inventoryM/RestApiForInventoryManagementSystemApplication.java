48b94663a32ad130d76cd35dc9b2bdab953c6ba5
azdine-dev <azeddinebenhalima@gmail.com>
2021-03-07 13:58:41 +0100

    first commit

---
+++ src/main/java/com/azdine/dev/inventoryM/RestApiForInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.azdine.dev.inventoryM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiForInventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azdine/dev/inventoryM/RestApiForInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azdine.dev.inventoryM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
